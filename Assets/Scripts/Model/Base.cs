1942bd4be69c60d677183edc6774cab145bc9490
DemocriF <fardin.democri@mail.mcgill.ca>
2025-03-13 23:02:44 -0400

    Made Base, Resources, working on Game

--- Assets/Scripts/Model/Base.cs
+++ Assets/Scripts/Model/Base.cs
@@ -7,10 +7,34 @@ namespace Assets.Scripts.Model
     [System.Serializable]
     public class Base
     {
-        public List<Base> bases;
+        private Dictionary<int, int> buildings = new Dictionary <int, int>();
+         
+        public class Building 
+        {
+            public Dictionary<int, (string name, string description)> building = new Dictionary <int, (string, string)>
+            {
+                { 0, ("Living Quarters", "Housing for soldiers")}, 
+                { 1, ("Hospital", "You can heal soldiers in Hospital")},
+                { 2, ("Lab", "Research and development")},
+                { 3, ("Hangar", "Facility for aircraft and vehicles")}
+            };
+
+        }
+ 
         public Base()
         {
-            bases = new List<Base>();
+            buildings.Add(0, 1);
+        }
+ 
+        public Base (List <int> ids, List <int> lvls)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (ids[i] != 0)
+                {
+                    buildings.Add(i, lvls[i]);
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Model/Game.cs
+++ Assets/Scripts/Model/Game.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Model;
 
 
 namespace Assets.Scripts.Model
@@ -8,12 +9,23 @@ namespace Assets.Scripts.Model
     [System.Serializable]
     public class Game
     {
-        
+         
         public Resources resourcesData;
-        public Mission missionData;
+        public List <Mission> MissionsData;
+        public List <Soldier> soldiersData;
+        public List <Base> basesData;
         public Tech techData;
-        public Base baseData;
-        
-    }
 
+        public Game()
+        {
+            this.resourcesData = new Resources();
+            this.MissionsData = new List <Mission> ();
+            this.soldiersData = new List <Soldier> ();
+            this.basesData = new List <Base> ();
+            this.basesData.Add(new Base());
+            this.techData = new Tech();
+        }        
+    }
+ 
+    
 }
