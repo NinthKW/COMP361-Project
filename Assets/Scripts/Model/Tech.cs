a0a2f702459382951f1f5ff19828a05762036c83
jadenhum <122134573+jadenhum@users.noreply.github.com>
2025-03-17 16:09:55 -0400

    create tech model

--- Assets/Scripts/Model/Tech.cs
+++ Assets/Scripts/Model/Tech.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 namespace Assets.Scripts.Model {
     [System.Serializable]
     public class Tech {
-        
+        public int techId;
+        public string techName;
+        public string description;
+        public float costMoney;
+        public int costResourceId;
+        public int costResourceAmount;
+        public bool isUnlocked;
+
+        public Tech(int techId, string techName, string description, float costMoney, 
+                   int costResourceId, int costResourceAmount) 
+        {
+            this.techId = techId;
+            this.techName = techName;
+            this.description = description;
+            this.costMoney = costMoney;
+            this.costResourceId = costResourceId;
+            this.costResourceAmount = costResourceAmount;
+            this.isUnlocked = false;
+        }
     }
 }
