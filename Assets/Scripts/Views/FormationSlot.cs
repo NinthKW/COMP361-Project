f4b8747aeea23b35207d06311eae4eedc5f4ed4b
Joe <yueqiao.zhang@mail.mcgill.ca>
2025-03-24 01:53:22 -0400

    First stage Soldier Selection done

--- Assets/Scripts/Views/FormationSlot.cs
+++ Assets/Scripts/Views/FormationSlot.cs
@@ -1,68 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Linq;
+using System.Collections.Generic;
 using Assets.Scripts.Model;
-public class FormationSlot : MonoBehaviour, IDropHandler
+
+
+public class FormationSlot : MonoBehaviour
 {
-    [SerializeField] private Image slotImage;
+    [SerializeField] private Image slotBackground;
+    [SerializeField] private Button slotButton;
     [SerializeField] private Button cancelButton;
-    [SerializeField] private GameObject characterUIPrefab;
+    [SerializeField] private Image soldierPortrait;
 
     public Soldier CurrentSoldier { get; private set; }
-    private int slotIndex;
     private MissionPreparationUI ui;
+    private int slotIndex;
 
-    public void Initialize(int index, MissionPreparationUI uiController)
+    public void Initialize(MissionPreparationUI uiController, int index)
     {
-        slotIndex = index;
         ui = uiController;
-        cancelButton.gameObject.SetActive(false);
+        slotIndex = index;
+        slotButton.onClick.AddListener(OnSlotClicked);
+        cancelButton.onClick.AddListener(OnCancelClicked);
+        UpdateVisuals();
+    }
+
+    void OnSlotClicked()
+    {
+        ui.OnSlotSelected(this);
+    }
+
+    void OnCancelClicked()
+    {
+        if (CurrentSoldier != null)
+        {
+            ui.ReturnSoldierToContainer(CurrentSoldier);
+            CurrentSoldier = null;
+            UpdateVisuals();
+        }
     }
 
     public void AssignSoldier(Soldier soldier)
     {
         CurrentSoldier = soldier;
-        var characterCard = Instantiate(characterUIPrefab, transform);
-        characterCard.GetComponent<SoldierCard>().Initialize(soldier, ui);
         UpdateVisuals();
-    }
-
-    public bool ContainsSoldier(Soldier soldier)
-    {
-        return CurrentSoldier == soldier;
-    }
-
-    public void ClearSlot()
-    {
-        CurrentSoldier = null;
-        foreach (Transform child in transform)
-        {
-            if (child.GetComponent<SoldierCard>()) Destroy(child.gameObject);
-        }
-        UpdateVisuals();
-    }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        var draggedPortrait = eventData.pointerDrag.GetComponent<SoldierCard>();
-        if (draggedPortrait != null)
-        {
-            ui.TryAssignSoldierToSlot(draggedPortrait.Soldier, slotIndex);
-        }
-    }
-
-    public void ToggleCancelButton(bool show)
-    {
-        cancelButton.gameObject.SetActive(show);
-        cancelButton.onClick.RemoveAllListeners();
-        if (show)
-        {
-            cancelButton.onClick.AddListener(() => ui.RemoveSoldierFromFormation(CurrentSoldier));
-        }
     }
 
     void UpdateVisuals()
     {
-        slotImage.color = CurrentSoldier == null ? Color.gray : Color.white;
+        // soldierPortrait.gameObject.SetActive(CurrentSoldier != null);
+        cancelButton.gameObject.SetActive(CurrentSoldier != null);
+
+        if (CurrentSoldier != null)
+        {
+            // 加载士兵头像
+            // soldierPortrait.sprite = ...
+        }
     }
 }
--- Assets/Scripts/Views/MissionPrepUI.cs
+++ Assets/Scripts/Views/MissionPrepUI.cs
@@ -10,27 +10,28 @@ using System.Linq;
 
 public class MissionPreparationUI : MonoBehaviour
 {
-    [Header("UI Components")]
+    [Header("UI References")]
     [SerializeField] private Transform availableSoldiersPanel;
     [SerializeField] private Transform availableEnemiesPanel;
     [SerializeField] private Transform formationSlotsPanel;
-    [SerializeField] private InfoPanel soldierInfoPanel;
-    [SerializeField] private InfoPanel enemyInfoPanel;
+    [SerializeField] private Button assignButton;
     [SerializeField] private Button startBattleButton;
     [SerializeField] private Button retreatButton;
 
+    [Header("Text References")]
+    [SerializeField] private InfoPanel soldierInfoPanel;
+    [SerializeField] private InfoPanel enemyInfoPanel;
+
     [Header("Prefabs")]
     [SerializeField] private GameObject soldierCardPrefab;
-    [SerializeField] private GameObject enemyCardPrefab;
     [SerializeField] private GameObject formationSlotPrefab;
 
+    private List<CharacterCard> characterCards = new List<CharacterCard>();
+    private List<CharacterCard> enemyCards = new List<CharacterCard>();
     private List<FormationSlot> formationSlots = new List<FormationSlot>();
-    private List<SoldierCard> availableSoldierCards = new List<SoldierCard>();
-    private List<CharacterUI> availableEnemyCards = new List<CharacterUI>();
+    private CharacterCard selectedCharacterCard;
+    private FormationSlot selectedFormationSlot;
 
-    private Soldier selectedSoldier;
-    private Enemy selectedEnemy;
-    
     void Start()
     {
         if (CombatManager.Instance == null)
@@ -44,123 +45,196 @@ public class MissionPreparationUI : MonoBehaviour
 
     void InitializeUI()
     {
-        int midX = Screen.width / 2;
-        int midY = Screen.height / 2;
-        int midAllyX = midX;
-        List<Vector2> allyPositions = new List<Vector2>
+        List<Vector3> positions = new()
         {
-            new(midAllyX - 200, midY - 200),
-            new(midAllyX - 200, midY),
-            new(midAllyX - 200, midY + 200),
-            new(midAllyX, midY - 100),
-            new(midAllyX, midY + 100),
+            new Vector3(-200, -100, 0),
+            new Vector3(-100, 100, 0),
+            new Vector3(0, -100, 0),
+            new Vector3(100, 100, 0),
+            new Vector3(200, -100, 0)
         };
-        // 初始化5个阵型位置
+        // 初始化阵型槽位
         for (int i = 0; i < 5; i++)
         {
             var slot = Instantiate(formationSlotPrefab, formationSlotsPanel).GetComponent<FormationSlot>();
-            slot.Initialize(i, this);
+            slot.Initialize(this, i);
+            slot.gameObject.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            
+            slot.gameObject.GetComponent<RectTransform>().localPosition = positions[i];
+            slot.gameObject.SetActive(true);
             formationSlots.Add(slot);
-            slot.transform.position = allyPositions[i];
         }
 
         // 加载可用士兵
         foreach (var soldier in CombatManager.Instance.GetAvailableSoldiers())
         {
-            var card = Instantiate(soldierCardPrefab, availableSoldiersPanel).GetComponent<SoldierCard>();
+            var card = Instantiate(soldierCardPrefab, availableSoldiersPanel).GetComponent<CharacterCard>();
             card.Initialize(soldier, this);
-            availableSoldierCards.Add(card);
+            characterCards.Add(card);
         }
 
-        // 加载敌人信息
+        // 加载敌人
         foreach (var enemy in CombatManager.Instance.GetAvailableEnemies())
         {
-            var card = Instantiate(enemyCardPrefab, availableEnemiesPanel).GetComponent<CharacterUI>();
+            var card = Instantiate(soldierCardPrefab, availableEnemiesPanel).GetComponent<CharacterCard>();
             card.Initialize(enemy, this);
-            availableEnemyCards.Add(card);
+            enemyCards.Add(card);
         }
     }
 
     void SetupButtons()
     {
+        assignButton.onClick.AddListener(OnAssignButtonClicked);
         startBattleButton.onClick.AddListener(OnStartBattle);
         retreatButton.onClick.AddListener(OnRetreat);
-    }
-
-    public void OnSoldierSelected(Soldier soldier)
-    {
-        selectedSoldier = soldier;
-        soldierInfoPanel.UpdateInfo(soldier);
-        
-        // 如果是从阵型中选择，显示取消按钮
-        formationSlots.ForEach(slot => slot.ToggleCancelButton(slot.ContainsSoldier(soldier)));
-    }
-
-    public void OnEnemySelected(Enemy enemy)
-    {
-        selectedEnemy = enemy;
-        enemyInfoPanel.UpdateInfo(enemy);
-    }
-
-    public void TryAssignSoldierToSlot(Soldier soldier, int slotIndex)
-    {
-        // 查找第一个空位或交换位置
-        var targetSlot = formationSlots[slotIndex];
-        if (targetSlot.CurrentSoldier == null)
-        {
-            AssignSoldierToSlot(soldier, targetSlot);
-        }
-        else
-        {
-            // 交换位置逻辑
-            var originalSlot = formationSlots.First(s => s.CurrentSoldier == soldier);
-            var tempSoldier = targetSlot.CurrentSoldier;
-            
-            targetSlot.AssignSoldier(soldier);
-            originalSlot.AssignSoldier(tempSoldier);
-        }
-    }
-
-    private void AssignSoldierToSlot(Soldier soldier, FormationSlot slot)
-    {
-        // 从原位置移除
-        var currentSlot = formationSlots.FirstOrDefault(s => s.CurrentSoldier == soldier);
-        currentSlot?.ClearSlot();
-        
-        // 分配到新位置
-        slot.AssignSoldier(soldier);
-        
-        // 更新卡牌状态
-        availableSoldierCards.First(c => c.Soldier == soldier).SetSelected(true);
-    }
-
-    public void RemoveSoldierFromFormation(Soldier soldier)
-    {
-        var slot = formationSlots.First(s => s.CurrentSoldier == soldier);
-        slot.ClearSlot();
-        availableSoldierCards.First(c => c.Soldier == soldier).SetSelected(false);
-    }
-
-    void OnStartBattle()
-    {
-        var selectedSoldiers = formationSlots
-            .Where(s => s.CurrentSoldier != null)
-            .Select(s => s.CurrentSoldier)
-            .ToList();
-
-        if (selectedSoldiers.Count == 0)
-        {
-            // Todo: pop up window
-            Debug.LogError("至少需要选择一个士兵！");
-            return;
-        }
-
-        CombatManager.Instance.StartCombat(selectedSoldiers, CombatManager.Instance.GetAvailableEnemies());
-        GameManager.Instance.LoadGameState(GameState.CombatPage);
+        UpdateButtonStates();
     }
 
     void OnRetreat()
     {
-        GameManager.Instance.LoadGameState(GameState.MissionSelectPage);
+        // 返回到关卡选择界面
+        GameManager.Instance.LoadGameState(GameState.MissionPage);
+    }
+
+    void OnStartBattle()
+    {
+        // 检查是否所有槽位都有士兵
+        if (formationSlots.Any(slot => slot.CurrentSoldier == null))
+        {
+            Debug.Log("请为所有槽位分配士兵");
+            return;
+        }
+
+        List<Soldier> combatSoldiers = new List<Soldier>();
+        List<Enemy> combatEnemies = new List<Enemy>();
+
+        foreach(Soldier soldier in formationSlots.Select(slot => slot.CurrentSoldier).ToList())
+        {
+            combatSoldiers.Add(soldier);
+        }
+
+        // 开始战斗
+        CombatManager.Instance.StartCombat(
+            formationSlots.Select(slot => slot.CurrentSoldier).ToList(), 
+            CombatManager.Instance.GetAvailableEnemies()
+        );
+
+        // 进入战斗场景
+        GameManager.Instance.LoadGameState(GameState.CombatPage);
+    }
+
+    public void OnSoldierSelected(CharacterCard card)
+    {
+        // 取消之前的选择
+        if (selectedCharacterCard != null) 
+        {
+            selectedCharacterCard.SetSelected(false);
+        }
+
+        selectedCharacterCard = card;
+        selectedFormationSlot = null;
+        
+        if (card != null)
+        {
+            card.SetSelected(true);
+        }
+        
+        soldierInfoPanel.UpdateInfo((Soldier) card.Character);
+        UpdateButtonStates();
+    }
+
+    public void OnEnemySelected(CharacterCard card)
+    {
+        // 取消之前的选择
+        if (selectedCharacterCard != null) 
+        {
+            selectedCharacterCard.SetSelected(false);
+        }
+
+        selectedCharacterCard = card;
+        selectedFormationSlot = null;
+        
+        if (card != null)
+        {
+            card.SetSelected(true);
+        }
+        
+        enemyInfoPanel.UpdateInfo((Enemy) card.Character);
+        UpdateButtonStates();
+    }
+
+
+    public void OnSlotSelected(FormationSlot slot)
+    {
+        selectedFormationSlot = slot;
+        UpdateButtonStates();
+    }
+
+    void UpdateButtonStates()
+    {
+        // Assign按钮状态
+        bool canAssign = selectedCharacterCard != null && selectedFormationSlot != null;
+        assignButton.interactable = canAssign;
+
+        // 其他按钮状态...
+    }
+
+    void OnAssignButtonClicked()
+    {
+        if (selectedFormationSlot == null || selectedCharacterCard == null) return;
+
+        // 如果槽位已有士兵，先取消
+        if (selectedFormationSlot.CurrentSoldier != null)
+        {
+            ReturnSoldierToContainer(selectedFormationSlot.CurrentSoldier);
+        }
+
+        // 分配新士兵
+        AssignSoldierToSlot((Soldier) selectedCharacterCard.Character, selectedFormationSlot);
+        
+        // 重置选择
+        ClearSelection();
+    }
+
+    void AssignSoldierToSlot(Soldier soldier, FormationSlot slot)
+    {
+        slot.AssignSoldier(soldier);
+        selectedCharacterCard.SetAssigned(true);
+    }
+
+    public void ReturnSoldierToContainer(Soldier soldier)
+    {
+        var card = characterCards.FirstOrDefault(c => c.Character == soldier);
+        if (card != null)
+        {
+            card.SetAssigned(false);
+        }
+    }
+
+    void ClearSelection()
+    {
+        selectedCharacterCard = null;
+        selectedFormationSlot = null;
+        UpdateButtonStates();
+    }
+}
+
+public class SoldierInfoPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI defenseText;
+    [SerializeField] private TextMeshProUGUI roleText;
+
+    public void UpdateInfo(Soldier soldier)
+    {
+        nameText.text = soldier.Name;
+        levelText.text = $"Level: {soldier.Level}";
+        healthText.text = $"HP: {soldier.Health}/{soldier.MaxHealth}";
+        attackText.text = $"ATK: {soldier.Atk}";
+        defenseText.text = $"DEF: {soldier.Def}";
+        roleText.text = $"Role: {soldier.GetRoleName()}";
     }
 }
