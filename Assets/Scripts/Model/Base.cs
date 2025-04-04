05c76794bac90136690a1d3e343d76f83c55025a
DemocriF <fardin.democri@mail.mcgill.ca>
2025-04-03 21:40:39 -0400

    minor fixes

--- Assets/Scripts/Model/Base.cs
+++ Assets/Scripts/Model/Base.cs
@@ -1,3 +1,5 @@
+using Codice.CM.WorkspaceServer.Tree;
+using log4net.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,39 +9,25 @@ namespace Assets.Scripts.Model
     [System.Serializable]
     public class Base
     {
-        private Dictionary<int, int> buildings = new Dictionary <int, int>();
-         
-        public class Building 
-        {
-            public Dictionary<int, (string name, string description)> building = new Dictionary <int, (string, string)>
-            {
-                { 0, ("Living Quarters", "Housing for soldiers")}, 
-                { 1, ("Hospital", "You can heal soldiers in Hospital")},
-                { 2, ("Lab", "Research and development")},
-                { 3, ("Hangar", "Facility for aircraft and vehicles")}
-            };
+        public int building_id;
+        public string name;
+        public string description;
+        public int level;
+        public int cost;
+        public int resource_amount;
+        public int resource_type;
+        public bool unlocked;
 
-        }
- 
-        public Base()
+        public Base(int building_id, string name, string description, int level, int cost, int resource_amount, int resource_type, bool unlocked)
         {
-            buildings.Add(0, 1);
-        }
- 
-        public Base (List <int> ids, List <int> lvls)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (ids[i] != 0)
-                {
-                    buildings.Add(i, lvls[i]);
-                }
-            }
-        }
-
-        public Dictionary<int, int> GetBuildings()
-        {
-            return buildings;
+            this.building_id = building_id;
+            this.name = name;
+            this.description = description;
+            this.level = level;
+            this.cost = cost;
+            this.resource_amount = resource_amount;
+            this.resource_type = resource_type;
+            this.unlocked = unlocked;
         }
     }
 }
