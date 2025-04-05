ea3ca44e57bfe2444410cc7f355895434462c7c5
jadenhum <122134573+jadenhum@users.noreply.github.com>
2025-04-05 15:12:16 -0400

    other tech stuff

--- Assets/Scripts/Views/TechUI.cs
+++ Assets/Scripts/Views/TechUI.cs
@@ -6,26 +6,43 @@ using Assets.Scripts.Model;
 using Assets.Scripts.Controller;
 using TMPro;
 
-
-
 public class TechUI : MonoBehaviour
 {
-    
     public Button exitButton;
+    public Button unlockButton;
+    public TechButton[] techButtons; // Array of all tech buttons
+    private UnlockButtonController unlockButtonController;
+    
+    void Awake()
+    {
 
-       
+        // Remove any existing UnlockButtonController components from the entire hierarchy
+        var controllers = GetComponentsInChildren<UnlockButtonController>(true);
+        foreach (var controller in controllers)
+        {
+            DestroyImmediate(controller);
+        }
+        
+        if (unlockButton != null)
+        {
+            unlockButtonController = unlockButton.gameObject.AddComponent<UnlockButtonController>();
+        }
+    }
 
     void Start()
     {
-        
         exitButton.onClick.AddListener(OnBackButtonClicked1);
+        
+        // Make sure TechManager is initialized
+        if (TechManager.Instance == null)
+        {
+            Debug.LogError("TechManager not found in scene!");
+        }
     }
 
-   
     void OnBackButtonClicked1()
     {
         GameManager.Instance.ChangeState(GameState.MainMenuPage);
         GameManager.Instance.LoadGameState(GameState.MainMenuPage);
     }
-
 }
