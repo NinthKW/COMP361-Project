5c44bd387cf9d2f4244a7365a7f988871d86377a
Jiaqi Yang <jiayang@outlook.fr>
2025-04-17 22:33:58 -0400

    added healing to resource display

--- Assets/Scripts/Views/ResourceDiaplayUi.cs
+++ Assets/Scripts/Views/ResourceDiaplayUi.cs
@@ -18,6 +18,8 @@ public class ResourceDisplayUI : MonoBehaviour
     public TextMeshProUGUI resourceHeaderTextObject;
 
     public string resourcesHeaderText = "Resources";
+    private float time = 0f;
+    private float interval = 1.0f;
 
     void Start()
     {
@@ -27,6 +29,16 @@ public class ResourceDisplayUI : MonoBehaviour
             resourceHeaderTextObject.text = resourcesHeaderText;
         }
         PopulateResources();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > interval)
+        {
+            time = 0f;
+            PopulateResources();
+        }
     }
 
     void PopulateResources()
@@ -40,8 +52,8 @@ public class ResourceDisplayUI : MonoBehaviour
         // Retrieve the resource data using your ResourceManager
         ModelResources resData = ResourceManager.Instance.GetResources();
         
-        // Iterate through resource IDs 0 to 4 (for Food, Money, Iron, Wood, Titanium)
-        for (int id = 0; id < 5; id++)
+        // Iterate through resource IDs 0 to 5 (for Food, Money, Iron, Wood, Titanium, Healing)
+        for (int id = 0; id <= 5; id++)
         {
             string resName = resData.GetName(id);
             int resAmount = resData.GetAmount(id);
